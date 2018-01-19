baff09fb0abca5d83ddbed117fbf52880420e92d
hsiehkl <st4465@gmail.com>
2018-01-19 16:52:41 +0800

    Add KVO

--- VideoPlayer/VideoPlayer/VideoPlayerViewController.swift
+++ VideoPlayer/VideoPlayer/VideoPlayerViewController.swift
@@ -15,7 +15,11 @@ class VideoPlayerViewController: UIViewController {
     let avPlayerLayer = AVPlayerLayer()
     let buttomView = UIView()
     let searchBar = UISearchBar()
-    
+    let playButton = UIButton()
+    let muteButton = UIButton()
+    var isPalying = true
+    var isMuted = false
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
@@ -34,21 +38,20 @@ class VideoPlayerViewController: UIViewController {
     
     func setUpButtomView() {
         
-        self.buttomView.frame = CGRect(x: 0, y: self.view.frame.height - 50, width: self.view.frame.width, height: 50)
+        self.buttomView.frame = CGRect(x: 0, y: self.view.frame.height - 44, width: self.view.frame.width, height: 44)
         
         buttomView.backgroundColor = Color.buttomViewColor
         
-        let playButton = UIButton()
-        playButton.setTitle("Pause", for: .normal)
-        playButton.frame = CGRect(x: 20, y: 10 , width: 50, height: 20)
+//        playButton.setTitle("Pause", for: .normal)
+        playButton.frame = CGRect(x: 30, y: 10 , width: 50, height: 20)
         playButton.tintColor = UIColor.white
-        
+        playButton.addTarget(self, action: #selector(playButtonTaped), for: .touchUpInside)
         buttomView.addSubview(playButton)
         
-        let muteButton = UIButton()
-        muteButton.setTitle("Mute", for: .normal)
-        muteButton.frame = CGRect(x: self.view.frame.width - 70, y: 10 , width: 50, height: 20)
+//        muteButton.setTitle("Mute", for: .normal)
+        muteButton.frame = CGRect(x: self.view.frame.width - 80, y: 10 , width: 60, height: 20)
         muteButton.tintColor = UIColor.white
+        muteButton.addTarget(self, action: #selector(muteButtonTapped), for: .touchUpInside)
         buttomView.addSubview(muteButton)
         
         self.view.addSubview(buttomView)
@@ -66,6 +69,61 @@ class VideoPlayerViewController: UIViewController {
         self.view.addSubview(searchBar)
         
     }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if keyPath == "rate" {
+            
+            if let rate = change?[.newKey] as? Float {
+                
+                if rate == 0.0 {
+
+                    isPalying = false
+                    
+                } else if rate == 1.0 {
+
+                    isPalying = true
+                }
+            }
+        }
+    }
+    
+    @objc func playButtonTaped() {
+    
+        if isPalying {
+
+            self.avPlayer.pause()
+            self.playButton.setTitle("Play", for: .normal)
+
+        } else {
+
+            self.avPlayer.play()
+            self.playButton.setTitle("Pause", for: .normal)
+
+        }
+
+    }
+    
+    @objc func muteButtonTapped() {
+        
+        if isMuted {
+            
+            self.avPlayer.isMuted = false
+            self.muteButton.setTitle("Mute", for: .normal)
+            self.isMuted = false
+            
+        } else {
+            
+            self.avPlayer.isMuted = true
+            self.muteButton.setTitle("Sound", for: .normal)
+            self.isMuted = true
+        }
+    }
+    
+    deinit {
+        self.avPlayerLayer.removeObserver(self, forKeyPath: "status")
+    }
+
 }
 
 extension VideoPlayerViewController: UISearchBarDelegate {
@@ -84,6 +142,10 @@ extension VideoPlayerViewController: UISearchBarDelegate {
         self.avPlayer = AVPlayer(url: url)
         self.avPlayerLayer.player = self.avPlayer
         self.view.layer.addSublayer(self.avPlayerLayer)
+        playButton.setTitle("Pause", for: .normal)
+        muteButton.setTitle("Mute", for: .normal)
+        
+        self.avPlayer.addObserver(self, forKeyPath: "rate", options: .new, context: nil)
         
         avPlayerLayer.frame = CGRect(x: 0, y: 76, width: self.view.frame.width, height: self.view.frame.height - 126)
         
